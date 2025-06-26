1a6367752f7db58dcb1f14cc43ea94b128f85fd7
Annaymongillo <112717447+Annaymongillo@users.noreply.github.com>
2025-06-26 06:46:12 -0400

    Update street_sign_downloader.py

--- code/downloaders/signals_markings_signs/street_sign_downloader.py
+++ code/downloaders/signals_markings_signs/street_sign_downloader.py
@@ -1,4 +1,4 @@
-from nyc_base_downloader import NYCDataDownloader
+from code.downloaders.nyc_base_downloader import NYCDataDownloader
 
 class StreetSignWorkOrdersDownloader(NYCDataDownloader):
     BASE_URL = "https://data.cityofnewyork.us/api/views/qt6m-xctn/rows.csv?accessType=DOWNLOAD"
